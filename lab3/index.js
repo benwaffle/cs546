1d8436b21d3b38decfd1e08435c457f0316c90bc
Ben Iofel <iofelben@gmail.com>
2018-02-11 18:13:22 -0500

    tests

--- lab3/index.js
+++ lab3/index.js
@@ -14,7 +14,26 @@ async function assertThrowsAsync(func) {
 async function main() {
   try {
     await assertThrowsAsync(() => fileData.getFileAsString())
-    assert(await fileData.getFileAsString('test.txt') === 'test\n')
+    await assertThrowsAsync(() => fileData.getFileAsJSON())
+    await assertThrowsAsync(() => fileData.saveStringToFile())
+    await assertThrowsAsync(() => fileData.saveStringToFile('asdf'))
+    await assertThrowsAsync(() => fileData.saveJSONToFile())
+    await assertThrowsAsync(() => fileData.saveJSONToFile('asdf'))
+
+    const testData = '1234567890'
+    await fileData.saveStringToFile('test.txt', testData)
+    assert(await fileData.getFileAsString('test.txt') === testData)
+    await fs.unlink('test.txt')
+
+    const testJson = {a:1, b:{c:2, d:3}}
+    await fileData.saveJSONToFile('test.json', testJson)
+    assert(await fileData.getFileAsString('test.json') === JSON.stringify(testJson))
+    const readJson = await fileData.getFileAsJSON('test.json')
+    assert(readJson.a === 1)
+    assert(readJson.b.c === 2)
+    assert(readJson.b.d === 3)
+    await fs.unlink('test.json')
+
     console.log('✓ all tests passed')
   } catch (e) {
     console.error(e)
