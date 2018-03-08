605ef6df9a05b35836c11d97f6e126a0b4113de4
Ben Iofel <iofelben@gmail.com>
2018-03-08 02:49:43 -0500

    more

--- lab7/app.js
+++ lab7/app.js
@@ -6,11 +6,6 @@ const db = require('./db')
 app.use(require('body-parser').json())
 app.use(require('morgan')('dev')) // log requests
 
-/** are all values of arr truthy? */
-function all(arr) {
-    if (!Array.isArray(arr)) throw 'all() only works on arrays'
-    return arr.reduce((a, b) => a && b)
-}
 
 // Responds with an array of all recipes in the format of {_id: RECIPE_ID, title: RECIPE_TITLE} 
 router.get('/', async (req, res) => {
@@ -26,33 +21,59 @@ router.get('/:id', async (req, res) => {
     }
 })
 
+function isRecipe(recipe, partial = false) {
+    /** are all values of arr truthy? */
+    function all(arr) {
+        if (!Array.isArray(arr)) throw 'all() only works on arrays'
+        return arr.reduce((a, b) => a && b)
+    }
+
+    /**
+     * magic function to assert we have the right type
+     * @type - type we want
+     * @val - value to check
+     * @cb - predicate for children if val has a .map() method,
+     *       otherwise just a function to call if the type check succeeds
+     */
+    function expect(type, val, cb) {
+        const check = (type === 'array') ? Array.isArray(val) : typeof val === type
+
+        if (check) { // if we have the type we want
+            if (cb) {
+                if (type === 'array') return all(val.map(cb)) // check all children of array
+                else return cb() // check children of object
+            }
+            else return true
+        } else if (partial) { // if we allow undefined
+            return typeof val === 'undefined'
+        } else { // if we don't allow undefined
+            return false
+        }
+    }
+
+    return typeof recipe === 'object'
+        && typeof recipe._id === 'undefined'
+        && expect('string', recipe.title)
+        && expect('array', recipe.ingredients, (ing) =>
+            expect('object', ing, () =>
+                expect('string', ing.name) && expect('string', ing.amount)
+            )
+        )
+        && expect('array', recipe.steps, (step) => expect('string', step))
+}
+
 // Creates a recipe with the supplied data in the request body, and returns the new recipe
 router.post('/', async (req, res) => {
     const recipe = req.body
 
-    if (!all([
-        typeof recipe === 'object',
-
-        typeof recipe._id === 'undefined',
-        typeof recipe.title === 'string',
-
-        Array.isArray(recipe.ingredients)
-        && all(recipe.ingredients.map(ing =>
-            typeof ing === 'object'
-            && all([typeof ing.name === 'string',
-                    typeof ing.amount === 'string'])
-        )),
-
-        Array.isArray(recipe.steps)
-        && all(recipe.steps.map(step => typeof step === 'string'))
-
-    ])) {
+    if (!isRecipe(recipe)) {
         res.status(400).json({error: 'invalid recipe submitted'})
-        return;
+        return
     }
 
     try {
-        res.json(await db.create(recipe))
+        const _id = await db.create(recipe)
+        res.json(await db.get(_id))
     } catch (e) {
         res.status(400).json(e)
     }
@@ -60,17 +81,46 @@ router.post('/', async (req, res) => {
 
 // Updates the specified recipe with by replacing the recipe with the new recipe content, and returns the updated recipe
 router.put('/:id', async (req, res) => {
+    const recipe = req.body
 
+    if (!isRecipe(recipe)) {
+        res.status(400).json({error: 'invalid recipe submitted'})
+        return
+    }
+
+    try {
+        await db.create({ _id: req.params.id, ...recipe })
+        res.json(await db.get(req.params.id))
+    } catch (e) {
+        res.status(400).json(e)
+    }
 })
 
 // Updates the specified recipe with only the supplied changes, and returns the updated recipe
 router.patch('/:id', async (req, res) => {
+    const recipe = req.body
 
+    if (!isRecipe(recipe, true)) {
+        res.status(400).json({error: 'invalid recipe submitted'})
+        return
+    }
+
+    try {
+        await db.patch({ _id: req.params.id, ...recipe })
+        res.json(await db.get(req.params.id))
+    } catch (e) {
+        res.status(400).json(e)
+    }
 })
 
 // Deletes the recipe and returns nothing
 router.delete('/:id', async (req, res) => {
-
+    try {
+        await db.delete(req.params.id)
+        res.status(200)
+    } catch (e) {
+        res.status(400).json(e)
+    }
 })
 
 app.use('/recipes', router)
--- lab7/db.js
+++ lab7/db.js
@@ -3,7 +3,9 @@ const MongoClient = require('mongodb').MongoClient
 const conn = MongoClient.connect('mongodb://localhost:27017')
 const uuid = require('uuid/v4')
 
-const coll = conn.then(c => c.db('lab7-recipes')).then(db => db.collection('recipes'))
+const coll = conn
+    .then(c => c.db('lab7-recipes'))
+    .then(db => db.collection('recipes'))
 
 module.exports = {
     async getAll() {
@@ -25,8 +27,26 @@ module.exports = {
         const res = await db.insert({ _id, ...recipe })
         if (res.error)
             throw {error: res.error}
-        if (res.insertedCount != 1)
-            throw {error: 'something bad happened... 0 or multiple elements were inserted'}
-        return this.get(_id)
+        return _id
+    },
+
+    async put(recipe) {
+        await this.delete(recipe._id)
+        return await this.create(recipe)
+    },
+
+    async patch(recipe) {
+        const db = await coll
+        const res = await db.updateOne({ _id: recipe._id }, { $set: recipe })
+        if (res.error)
+            throw {error: res.error}
+    },
+
+    async delete(_id) {
+        const res = await db.deleteOne({ _id })
+        if (res.error)
+            throw {error: res.error}
+        if (res.result.deletedCount != 1)
+            throw {error: 'something bad happened'}
     }
 }
