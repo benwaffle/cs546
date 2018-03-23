96e9c2514855469d7b0836ba3707e21e4f415ffc
Ben Iofel <iofelben@gmail.com>
2018-03-23 15:23:54 -0400

    wip

---
+++ lab8/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+
+app.use(require('morgan')('dev'))
+app.use(require('body-parser').urlencoded({ extended: false }))
+app.set('view engine', 'hbs')
+
+app.use('/public', express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.post('/result', (req, res) => {
+    const str = req.body['text-to-test']
+    const clean = str.replace(/[^\w]/g, '')
+
+    res.render('result', {
+        text: str,
+        className:
+            (clean === clean.split('').reverse().join(''))
+            ? 'success'
+            : 'failure'
+    })
+})
+
+app.listen(3000, () => console.log('http://localhost:3000'))
