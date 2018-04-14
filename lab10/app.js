54a113158deddc41c9a5c4402e375b5790714194
Ben Iofel <iofelben@gmail.com>
2018-04-14 19:28:50 -0400

    works

--- lab10/app.js
+++ lab10/app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+const users = require('./users')
+
+const bcrypt = require('bcrypt')
+
 app.set('view engine', 'hbs')
 
 app.use(require('morgan')('dev'))
@@ -13,14 +17,61 @@ app.use(require('express-session')({
 }))
 
 app.get('/', (req, res) => {
-    if (req.session.loggedin)
+    if (req.session.user)
         res.redirect('/private')
     else
         res.render('login')
 })
 
-app.post('/login', (req, res) => {
-    
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body
+    if (typeof username === 'undefined' ||
+        typeof password === 'undefined') {
+            return;
+        }
+
+    if (username in users && await bcrypt.compare(password, users[username].hashedPassword)) {
+        req.session.user = username
+        res.redirect('/private')
+    } else {
+        res.render('login', {
+            failed: true
+        })
+    }
+})
+
+function loggedIn(req, res, next) {
+    const { user } = req.session
+    if (user)
+        next()
+    else
+        res.status(403).render('notloggedin')
+}
+
+app.get('/private', loggedIn, (req, res) => {
+    res.render('userinfo', {
+        ...users[req.session.user],
+        username: req.session.user,
+    })
+})
+
+app.get('/logout', (req, res) => {
+    req.session.cookie.maxAge = -1000*1000
+    delete req.session.user
+
+    res.send(`
+<!doctype html>
+    console.log(req.session)
+<html>
+    <head>
+        <title>Logged out</title>
+    </head>
+    <body>
+        You have been logged out
+        <a href="/">Go home</a>
+    </body>
+</html>
+`)
 })
 
 app.listen(3000, () => {
